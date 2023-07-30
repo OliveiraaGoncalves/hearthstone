4255c60eba236a5720022160f008cef35aa2d769
marcosoliveira <nonato.729@gmail.com>
2023-07-30 15:15:51 -0400

    [ISSUE-1] - Habilitando catalogs ao projeto

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
