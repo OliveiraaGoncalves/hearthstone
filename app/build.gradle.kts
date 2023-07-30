57068189f14debdb4e1682157f0df8590efab028
marcosoliveira <nonato.729@gmail.com>
2023-07-30 17:48:34 -0400

    [ISSUE-2] - Habilitando catalogs ao projeto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,8 @@ dependencies {
     implementation(libs.androidx.core)
     implementation(libs.androidx.appcompat)
     implementation(libs.google.material)
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.ui)
 //    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.junit.ext)
